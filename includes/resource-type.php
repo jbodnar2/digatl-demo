37dfa521f1145e890ca3fcd7fcb38fe5867bfa4c
Jon Bodnar <jbodnar2@gsu.edu>
2022-11-20 12:23:49 -0500

    Mess with admin, editor screens

--- includes/resource-type.php
+++ includes/resource-type.php
@@ -4,6 +4,7 @@
 Resource type demo
 */
 
+// Register CPT resource
 add_action("init", "register_resource_type");
 
 function register_resource_type()
@@ -25,6 +26,16 @@ function register_resource_type()
         "remove_featured_image" => "Remove Resource Image",
     ];
 
+    $capabilities = [
+        "edit_post" => "edit_resource",
+        "edit_posts" => "edit_resources",
+        "edit_others_posts" => "edit_other_resources",
+        "publish_posts" => "publish_resources",
+        "read_post" => "read_resource",
+        "read_private_posts" => "read_private_resources",
+        "delete_post" => "delete_resource",
+    ];
+
     $settings = [
         "supports" => [
             "editor",
@@ -39,13 +50,16 @@ function register_resource_type()
         "description" =>
             "A project, dataset, digital colleciton, or other online resource",
         "menu_position" => 20,
-        "taxonomies" => ["category", "post_tag", "resource_format"],
+        "taxonomies" => ["category", "post_tag", "format"],
+        "capabilities" => $capabilities,
+        "map_meta_cap" => true,
     ];
 
     register_post_type("resource", $settings);
 
     unset($settings);
     unset($labels);
+    unset($capabilities);
 }
 
 // Set up "default" custom fields/metadata on initial resource creation
@@ -71,10 +85,10 @@ function digatl_resource_default_metadata()
     return true;
 }
 
-// Register the resource_format taxonomy. Use hierarchical to allow checkbox format
-add_action("init", "register_resource_format_taxonomies");
+// Register the resourse taxonomies: formats, categories, and tags.
+add_action("init", "register_format_taxonomies");
 
-function register_resource_format_taxonomies()
+function register_format_taxonomies()
 {
     $labels = [
         "name" => "Formats",
@@ -140,3 +154,54 @@ function register_resource_format_taxonomies()
     unset($default_categories);
     unset($default_tags);
 }
+
+add_action("init", "modify_editor_administrator_caps");
+
+function modify_editor_administrator_caps()
+{
+    $editor = get_role("editor");
+    $administrator = get_role("administrator");
+
+    // Issue: Seems also to disable ability to use tags, categories.
+    // $caps = ["edit_pages", "publish_pages", "edit_posts", "publish_posts"];
+
+    // foreach ($caps as $cap) {
+    //     $editor->remove_cap($cap);
+    // }
+
+    $resource_caps = [
+        "edit_resource",
+        "edit_resources",
+        "edit_other_resources",
+        "publish_resources",
+        "read_resource",
+        "read_private_resources",
+        "delete_resource",
+    ];
+
+    foreach ($resource_caps as $cap) {
+        $administrator->add_cap($cap);
+        $editor->add_cap($cap);
+    }
+}
+
+// Quick fix for removing posts, pages from editor screen
+function digatl_check_if_editor()
+{
+    $user = wp_get_current_user();
+
+    if (in_array("editor", (array) $user->roles)) {
+        return true;
+    }
+    return false;
+}
+
+add_action("admin_menu", function () {
+    $is_editor = digatl_check_if_editor();
+    if ($is_editor) {
+        remove_menu_page("edit.php");
+        remove_menu_page("edit.php?post_type=page");
+        remove_menu_page("tools.php");
+        remove_menu_page("edit-comments.php");
+    }
+});
